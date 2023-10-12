2fd64a98ea31375d9d9d3e73949284af532cbed4
Amir <amk.64.128@gmail.com>
2023-10-12 11:45:54 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let sec = document.querySelector('.sec');
 let min = document.querySelector('.min');
+let hour = document.querySelector('.hour');
 
 let deg = 6;
 sec.style.transform = "rotateZ(deg)";
@@ -8,10 +9,11 @@ setInterval(handler , 1000);
 
 function handler(){
     let d = new Date();
-    let s = d.getSeconds();
-    let m = d.getMinutes();
-    
-    sec.style.transform = `rotateZ(${s*6}deg)`;
-    min.style.transform = `rotateZ(${m*6}deg)`;
+    let s = d.getSeconds() * deg;
+    let m = d.getMinutes() * deg;
+    let h = d.getHours() * 30;
+    sec.style.transform = `rotateZ(${s}deg)`;
+    min.style.transform = `rotateZ(${m}deg)`;
+    hour.style.transform = `rotateZ(${h+(m/12)}deg)`;
     
 }
